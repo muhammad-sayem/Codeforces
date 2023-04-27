d15c18e3d3ce6f4063b8b97e479b88cb3d986444
muhammad-sayem <shahrulislamsayem@gmail.com>
2023-04-27 14:32:46 +0600

    first commit

---
+++ 1_Basic_template.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+#define    endl      '\n'
+#define    yes       "YES"
+#define    no        "NO"
+#define    ll        long long
+#define    fastIO    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+using namespace std;
+int main()
+{
+    fastIO;
+
+
+    return 0;
+}
+
