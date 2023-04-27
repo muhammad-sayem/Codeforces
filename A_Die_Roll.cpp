584ed936dd5a193ae508d6c4225eb045e13a7a9b
muhammad-sayem <shahrulislamsayem@gmail.com>
2023-04-27 22:34:49 +0600

    Codeforces Beta Round 9 (Div. 2 Only) - A

---
+++ A_Die_Roll.cpp
@@ -0,0 +1,59 @@
+#include<bits/stdc++.h>
+#define    endl      '\n'
+#define    yes       "YES"
+#define    no        "NO"
+#define    ll        long long
+#define    fastIO    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+using namespace std;
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+
+    int maxx = max(a,b);
+    int diff = 6-maxx+1;
+
+    if(diff == 1) cout << "1/6" << endl;
+    else if(diff == 2) cout << "1/3" << endl;
+    else if(diff == 3) cout << "1/2" << endl;
+    else if(diff == 4) cout << "2/3" << endl;
+    else if(diff == 5) cout << "5/6" << endl;
+    else if(diff == 6) cout << "1/1" << endl;
+
+    return 0;
+}
+
+// ---------------------------------------------------------------------- //
+
+// Asif Abdullah's code //
+
+/*
+#include<bits/stdc++.h>
+#define   ll         long long
+#define   pii        pair<int,int>
+#define   sz(v)      v.size()
+#define   mem(a,x)   memset(a,x,sizeof(a))
+#define   fio        ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+using namespace std;
+int main()
+{
+    fio;
+
+    int a,b;
+    cin >> a >>b;
+
+    int mx = max(a,b);
+
+    int numerator = 6-mx+1;
+    int dominator = 6;
+
+    if(dominator%numerator==0) cout << numerator/numerator<<"/"<< dominator/numerator<< endl;
+    else if(dominator%2==0 && numerator%2==0) cout << numerator/2<<"/"<< dominator/2<< endl;
+    else cout << numerator << "/"<< dominator<< endl;
+
+    return 0;
+}
+
+
+
+*/
