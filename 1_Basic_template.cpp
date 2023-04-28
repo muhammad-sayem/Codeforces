e8390e71a95f3ca6339d0385ead9127b57449ccc
muhammad-sayem <shahrulislamsayem@gmail.com>
2023-04-28 15:35:12 +0600

    Basic Template

--- 1_Basic_template.cpp
+++ 1_Basic_template.cpp
@@ -3,6 +3,7 @@
 #define    yes       "YES"
 #define    no        "NO"
 #define    ll        long long
+#define    pii       pair <int,int>
 #define    fastIO    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 using namespace std;
 int main()
