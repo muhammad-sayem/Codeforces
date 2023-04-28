d8038e92621fc806c4fc82636e8aa7227ba35b43
muhammad-sayem <shahrulislamsayem@gmail.com>
2023-04-28 19:52:17 +0600

    Basic Template

--- 1_Basic_template.cpp
+++ 1_Basic_template.cpp
@@ -1,14 +1,18 @@
 #include<bits/stdc++.h>
-#define    endl      '\n'
-#define    yes       "YES"
-#define    no        "NO"
-#define    ll        long long
-#define    pii       pair <int,int>
-#define    fastIO    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define      endl            '\n'
+#define      yes             "YES"
+#define      no              "NO"
+#define      ll              long long
+#define      pii             pair <int,int>
+#define      pb              push_back
+#define      gcd(a,b)        __gcd(a,b)
+#define      lcm(a,b)        (a*b)/gcd(a,b)
+#define      fastIO          ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 using namespace std;
 int main()
 {
     fastIO;
+
 
     return 0;
 }
