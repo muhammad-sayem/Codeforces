4275f66293baaafd8c9b2b51dc9234c5cde3d2a2
muhammad-sayem <shahrulislamsayem@gmail.com>
2023-04-29 18:00:56 +0600

    Codeforces Round 827 (Div. 4) - 1742 C

--- C_Stripes.cpp
+++ C_Stripes.cpp
@@ -47,3 +47,58 @@ int main()
 
     return 0;
 }
+
+
+
+/*
+
+#include <bits/stdc++.h>
+#define ll long long
+#define pii pair<int, int>
+#define sz(v) v.size()
+#define mem(a, x) memset(a, x, sizeof(a))
+#define fio                       \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0)
+using namespace std;
+int main()      //taken help from Coding Community | Newton School && asif_abdullah's code ///
+{
+    fio;
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        char s[10][10];
+
+        for (int i = 1; i <= 8; i++)
+        {
+            for (int j = 1; j <= 8; j++)
+            {
+                cin >> s[i][j];
+            }
+        }
+
+        int cnt_r;
+
+        for (int i = 1; i <= 8; i++)
+        {
+            cnt_r=0;
+            for (int j = 1; j <= 8; j++)
+            {
+                if(s[i][j]=='R') cnt_r++;       
+            }
+
+            if(cnt_r==8) break;
+        }
+
+        if(cnt_r==8) cout << "R\n";
+        else cout << "B\n";
+        
+    }
+
+    return 0;
+}
+
+*/
