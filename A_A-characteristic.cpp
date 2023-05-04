94b210b84951adf5e7e4c1751c20f46a80b6fd81
muhammad-sayem <shahrulislamsayem@gmail.com>
2023-05-04 15:19:00 +0600

    Codeforces Round 868 (Div. 2) - 1823 A

--- A_A-characteristic.cpp
+++ A_A-characteristic.cpp
@@ -6,18 +6,45 @@
 #define    pii       pair <int,int>
 #define    fastIO    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 using namespace std;
+
+int sumReturn(int n){
+    return n*(n-1)/2;       /// n er maan joto tar theke ek kom er ek theke n porjonto value er jogfol ///
+}
+
 int main()
 {
-    fastIO;
+    fastIO;                    /// Abid vai ///
 
     int test;
     cin >> test;
 
     while(test--){
-        int n, k;
+        int n, k, a, b, i, flag = 0;
         cin >> n >> k;
 
-        
+        for(i=0; i<=n; i++){
+            if(sumReturn(i) + sumReturn(n-i) == k){
+                flag = 1;
+                a = i;
+                b = n-i;
+            }
+        }
+
+        if(flag) {
+            cout << yes << endl;
+            
+            for(i=0; i<a; i++){
+                cout << 1 << ' ';
+            }
+
+            for(i=0; i<b; i++){
+                cout << -1 << ' ';
+            }
+
+            cout << endl;
+        }
+
+        else cout << no << endl;
 
     }
 
