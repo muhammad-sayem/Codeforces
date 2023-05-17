cc1dd85c57f1e15095164d0e49fe1763c70fe79d
muhammad-sayem <shahrulislamsayem@gmail.com>
2023-05-17 22:13:21 +0600

    Codeforces Round 609 (Div. 2) - 1269 A

---
+++ A_Equation.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+#define      endl            '\n'
+#define      yes             "YES"
+#define      no              "NO"
+#define      ll              long long
+#define      sz(s)           s.size()
+#define      pii             pair <int,int>
+#define      pb              push_back
+#define      gcd(a,b)        __gcd(a,b)
+#define      lcm(a,b)        (a*b)/gcd(a,b)
+#define      fastIO          ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+using namespace std;
+int main()
+{
+    fastIO; 
+
+    int n;
+    cin >> n;
+
+    if(n%2 == 0) cout << n+4 << " " << 4 << endl;
+    else cout << n+9 << " " << 9 << endl;
+                 
+    return 0;
+}
+
+
+
