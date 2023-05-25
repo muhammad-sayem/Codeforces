a5b1d654bd90553b0edd22351fe9c2fd66ebb9f9
muhammad-sayem <shahrulislamsayem@gmail.com>
2023-05-26 00:07:36 +0600

    Educational Codeforces Round 149 (Rated for Div. 2) - 1837 A

---
+++ A_Grasshopper_on_a_Line.cpp
@@ -0,0 +1,85 @@
+#include<bits/stdc++.h>
+#define      endl            '\n'
+#define      yes             "YES"
+#define      no              "NO"
+#define      ll              long long
+#define      pb              push_back
+#define      pii             pair <int,int>
+#define      pll             pair <long long,long long>
+#define      rev(a)          reverse(a, a+n);
+#define      sort(a)         sort(a, a+n);
+#define      Srev(s)         reverse(s.begin(), s.end());
+#define      sz(s)           s.size()
+#define      gcd(a,b)        __gcd(a,b)
+#define      lcm(a,b)        (a*b)/gcd(a,b)
+#define      fastIO          ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+using namespace std;
+int main()
+{
+    fastIO;
+
+    int test;
+    cin >> test;
+
+    while(test--){
+        int n, k;
+        cin >> n >> k;
+
+        if(n<k){
+            cout << 1 << endl << n << endl;
+        }
+        else if(n%k == 0){
+            cout << 2 << endl;
+            cout << n-1 << " " << 1 << endl;
+        }
+        else{
+            cout << 1 << endl << n << endl;
+        }
+
+    }
+
+    return 0;
+}
+
+// -------------------------------------------------------------------- //
+
+/*
+#include<bits/stdc++.h>
+#define      endl            '\n'
+#define      yes             "YES"
+#define      no              "NO"
+#define      ll              long long
+#define      pb              push_back
+#define      pii             pair <int,int>
+#define      pll             pair <long long,long long>
+#define      rev(a)          reverse(a, a+n);
+#define      sort(a)         sort(a, a+n);
+#define      Srev(s)         reverse(s.begin(), s.end());
+#define      sz(s)           s.size()
+#define      gcd(a,b)        __gcd(a,b)
+#define      lcm(a,b)        (a*b)/gcd(a,b)
+#define      fastIO          ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+using namespace std;
+int main()
+{
+    fastIO;
+
+    int test;
+    cin >> test;
+
+    while(test--){
+        int n, k;
+        cin >> n >> k;
+
+        if(n<k || n%k != 0){
+            cout << 1 << endl << n << endl;
+        }
+        else{
+            cout << 2 << endl;
+            cout << n-1 << " " << 1 << endl;
+        }
+    }
+
+    return 0;
+}
+*/
