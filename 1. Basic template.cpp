aa80b2cfeff7082b2ad707af56ffced58d2abdb1
muhammad-sayem <shahrulislamsayem@gmail.com>
2023-06-05 01:54:26 +0600

    Basic Template

--- 1. Basic template.cpp
+++ 1. Basic template.cpp
@@ -7,7 +7,6 @@
 #define      pii             pair <int,int>
 #define      pll             pair <long long,long long>
 #define      rev(a)          reverse(a, a+n);
-#define      sort(a)         sort(a, a+n);
 #define      Srev(s)         reverse(s.begin(), s.end());
 #define      sz(s)           s.size()
 #define      gcd(a,b)        __gcd(a,b)
