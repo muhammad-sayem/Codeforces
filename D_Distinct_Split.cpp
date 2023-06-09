db18c6cdd0e040ec19298efcffc544dc596d5f25
muhammad-sayem <shahrulislamsayem@gmail.com>
2023-06-09 18:01:07 +0600

    Codeforces Round 849 (Div. 4) - 1791 D

--- D_Distinct_Split.cpp
+++ D_Distinct_Split.cpp
@@ -15,8 +15,43 @@
 using namespace std;
 int main()
 {
-    fastIO;
+    fastIO;                 // Credit: Redoan vai's code //              
 
+    int test;
+    cin >> test;
+
+    while(test--){
+        int n, i;
+        cin >> n;
+
+        string str;
+        cin >> str;
+
+        int a[n+5];
+        int b[n+5];
+
+        set<char> l, r;
+
+        for(i=0; i<n; i++){
+            l.insert(str[i]);             // 'l' set er moddhe first theke las porjonto unique elements add kora hocche //
+            a[i] = l.size();
+        }
+
+        for(i=n-1; i>=0; i--){
+            r.insert(str[i]);             // 'r' set er moddhe first theke las porjonto unique elements add kora hocche //
+            b[i] = r.size();
+        }
+
+        int maxx = -1e9;
+        int val;
+
+        for(i=0; i<n-1; i++){
+            val = a[i] + b[i+1];
+            maxx = max(maxx, val);
+        }
+
+        cout << maxx << endl;
+    }
 
     return 0;
 }
