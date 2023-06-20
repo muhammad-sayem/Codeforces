b2476360d7eb08a8e397c86e6ab5775595aa90fa
muhammad-sayem <shahrulislamsayem@gmail.com>
2023-06-21 00:10:15 +0600

    Codeforces Round 881 (Div. 3) - 1843 A

---
+++ C_Sum_in_Binary_Trees.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+#define      endl            '\n'
+#define      yes             "YES"
+#define      no              "NO"
+#define      ll              long long
+#define      ull             unsigned long long
+#define      pb              push_back
+#define      pii             pair <int,int>
+#define      pll             pair <long long,long long>
+#define      rev(a)          reverse(a, a+n);
+#define      Srev(s)         reverse(s.begin(), s.end());
+#define      sz(s)           s.size()
+#define      gcd(a,b)        __gcd(a,b)
+#define      lcm(a,b)        (a*b)/gcd(a,b)
+#define      fastIO          ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+using namespace std;
+int main()
+{
+    fastIO;
+
+    int test;
+    cin >> test;
+
+    while(test--){
+        ll n, sum = 0;
+        cin >> n;
+
+        while(n != 1){
+            sum += n;
+            n/=2;
+        }
+        cout << sum+1 << endl;
+    }
+
+    return 0;
+}
