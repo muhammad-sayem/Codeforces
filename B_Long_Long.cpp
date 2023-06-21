0c012a01ed54c4d25461b39708a8153c02789990
muhammad-sayem <shahrulislamsayem@gmail.com>
2023-06-22 02:31:43 +0600

    Codeforces Round 881 (Div. 3) - 1843 B

--- B_Long_Long.cpp
+++ B_Long_Long.cpp
@@ -15,21 +15,41 @@
 using namespace std;
 int main()
 {
-    fastIO;
+    fastIO;             // code idea from asif_abdullah //
 
     int test;
     cin >> test;
 
     while(test--){
-        ll n, i;
+        ll n, i, sum = 0;
         cin >> n;
         ll a[n+5];
 
         for(i=0; i<n; i++){
             cin >> a[i];
+            sum += abs(a[i]);
         }
-    }
 
+        int neg = 0;
+        int maxx = 0;
+
+        for(i=0; i<n; i++){
+            if(a[i] == 0) continue;
+            
+            else if(a[i] < 0) neg++;
+            
+            else{
+                if(neg > 0) {
+                    maxx++;
+                }
+                neg = 0;
+            }
+        }
+
+        if(neg > 0) maxx++;
+
+        cout << sum << " " << maxx << endl;
+    }
 
     return 0;
 }
