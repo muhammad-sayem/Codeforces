cb7c7e1d74c68995e18189aae263474e590dff22
muhammad-sayem <shahrulislamsayem@gmail.com>
2023-06-21 18:44:31 +0600

    Codeforces Round 479 (Div. 3) - 977 B

---
+++ B_Two-Gram.cpp
@@ -0,0 +1,68 @@
+#include<bits/stdc++.h>
+#define      endl            '\n'
+#define      yes             "YES"
+#define      no              "NO"
+#define      ll              long long
+#define      pb              push_back
+#define      pii             pair <int,int>
+#define      pll             pair <long long,long long>
+#define      rev(a)          reverse(a, a+n);
+#define      Srev(s)         reverse(s.begin(), s.end());
+#define      sz(s)           s.size()
+#define      gcd(a,b)        __gcd(a,b)
+#define      lcm(a,b)        (a*b)/gcd(a,b)
+#define      fastIO          ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+using namespace std;
+int main()
+{
+    fastIO;
+
+    int n, i, j;
+    cin >> n;
+
+    string str;
+    cin >> str;
+
+    if(n == 2) {
+        cout << str << endl;
+        return 0;
+    }
+
+    vector<string> v;
+
+    for(i=0; i<n-1; i++){
+        string s;
+
+        s.pb(str[i]);
+        s.pb(str[i+1]);
+
+        v.pb(s);
+        
+        s.clear();
+    }
+
+    // sort(v.begin(), v.end());
+
+    int maxx = -1, cnt;
+    string store;
+
+    for(i=0; i<v.size()-1; i++){
+        cnt = 1;
+
+        for(j=i+1; j<v.size(); j++){
+            if(v[i] == v[j]){
+                cnt++;
+            }
+        }
+        
+        if(cnt>=maxx){
+            maxx = max(cnt, maxx);
+            store = v[i];
+        }
+        
+    }
+
+    cout << store << endl;
+
+    return 0;
+}
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,5 @@
+    if(cnt > maxx){
+                maxx = max(cnt, maxx);
+                store = v[i];
+                cnt = 1;    
+            }
