34431ea7728bbee884b2330b7f08ab9914e48091
muhammad-sayem <shahrulislamsayem@gmail.com>
2023-07-02 16:44:51 +0600

    Codeforces Round 773 (Div. 2) - 1642 B

---
+++ B_Power_Walking.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+#define      endl            '\n'
+#define      yes             "YES"
+#define      no              "NO"
+#define      ll              long long
+#define      pb              push_back
+#define      pii             pair <int,int>
+#define      pll             pair <long long,long long>
+#define      rev(a)          reverse(a, a+n);
+#define      Srev(s)         reverse(s.begin(), s.end());
+#define      sz(s)           s.size()
+#define      gcd(a,b)        __gcd(a,b)
+#define      lcm(a,b)        (a*b)/gcd(a,b)
+#define      fastIO          ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+using namespace std;
+int main()
+{
+    fastIO;
+
+    int test;
+    cin >> test;
+
+    while(test--){
+        int n, i;
+        cin >> n;
+        int a[n+5];
+        
+        set<int> s;
+
+        for(i=1; i<=n; i++){
+            cin >> a[i];
+            s.insert(a[i]);
+        }
+
+        int unique = s.size();
+
+        for(i=1; i<=n; i++){
+            if(i<=unique) cout << unique << " ";
+            else cout << i << " ";
+        }
+
+        cout << endl;
+
+    }
+
+    return 0;
+}
