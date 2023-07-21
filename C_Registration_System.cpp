44f99e8267515793f6fbd058e4fcf7c1d1b5d82c
muhammad-sayem <shahrulislamsayem@gmail.com>
2023-07-22 00:26:26 +0600

    Codeforces Beta Round 4 (Div. 2 Only) - 4C

---
+++ C_Registration_System.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+#define      endl            '\n'
+#define      yes             "YES"
+#define      no              "NO"
+#define      ll              long long
+#define      pb              push_back
+#define      pii             pair <int,int>
+#define      pll             pair <long long,long long>
+#define      rev(a)          reverse(a, a+n);
+#define      Srev(s)         reverse(s.begin(), s.end());
+#define      sz(s)           s.size()
+#define      gcd(a,b)        __gcd(a,b)
+#define      lcm(a,b)        (a*b)/gcd(a,b)
+#define      fastIO          ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+using namespace std;
+int main()
+{
+    fastIO;
+
+    int n;
+    cin >> n;
+    map<string, int> mp;
+
+    while(n--){
+        string str;
+        cin >> str;
+
+        mp[str]++;
+
+        if(mp[str] == 1) cout << "OK" << endl;
+        else cout << str << mp[str]-1 << endl;
+    }
+
+    return 0;
+}
