4992c7044e498a74246796309b7285d01bbf0659
muhammad-sayem <shahrulislamsayem@gmail.com>
2023-07-30 00:23:09 +0600

    Codeforces Round 889 (Div. 2) - 1855 B

--- B_Longest_Divisor_Interval.cpp
+++ B_Longest_Divisor_Interval.cpp
@@ -55,6 +55,62 @@ int main()
     return 0;
 }
 
+// -------------------------------------------------------------------------- //
+
+/*
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fast                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);
+#define Print(_Q) for(int i=0;i<_Q.size();i++){if(i+1==_Q.size())cout<<_Q[i]<<"\n";else cout<<_Q[i]<<" ";}
+#define Sort(_Q) sort(_Q.begin(),_Q.end())
+#define INF  9223372036854775806
+#define MINF -9223372036854775806
+#define sz(x) (ll)x.size()
+#define gcd __gcd
+#define yes "YES"
+#define no  "NO"
+#define pb  push_back
+#define in  " "
+#define mone "-1"
+#define zero "0"
+#define one "1"
+#define nl "\n"
+pair<ll,ll>CORDINATE[8]= {{1,0},{-1,0},{0,1},{0,-1},{1,1},{1,-1},{-1,1},{-1,-1}};
+const ll mod=1e9+7;
+const double pi=2*acos(0.0);
+ll lcm(ll a,ll b)
+{
+    return (a/(__gcd(a, b)))*b;
+}
+const ll mxn=2e5+10;
+vector<ll>g1[mxn+10],g2[mxn+10];
+
+int main()
+{
+    fast
+    ll t;
+    cin>>t;                 // legend shehenulislam vai //
+    while(t--)
+    {
+        ll n;
+        cin>>n;
+        ll i=1;
+        ll ans=0;
+        while(n%i==0)
+        {
+            ans++;
+            i++;
+        }
+        cout<<ans<<nl;
+    }
+    return 0;
+}
+
+*/
+
  // ----------------------------------------------------------------------- //
 
 /*
