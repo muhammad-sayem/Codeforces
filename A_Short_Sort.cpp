5b56380ecb83f3c1e205da3a4769b891df8fa645
muhammad-sayem <shahrulislamsayem@gmail.com>
2023-09-22 00:49:19 +0600

    Codeforces Round 898 (Div. 4) - 1873 A

---
+++ A_Short_Sort.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+#define      endl            '\n'
+#define      yes             "YES"
+#define      no              "NO"
+#define      ll              long long
+#define      pb              push_back
+#define      pii             pair <int,int>
+#define      pll             pair <long long,long long>
+#define      rev(a)          reverse(a, a+n);
+#define      Srev(s)         reverse(s.begin(), s.end());
+#define      sz(s)           s.size()
+#define      gcd(a,b)        __gcd(a,b)
+#define      lcm(a,b)        (a*b)/gcd(a,b)
+#define      fastIO          ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+using namespace std;
+int main()
+{
+    fastIO;
+
+    int test;
+    cin >> test;
+
+    while(test--){
+        string s;
+        cin >> s;
+
+        if(s == "abc"  ||  s == "acb" || s == "cba" || s == "bac") cout << yes << endl;
+        else cout << no << endl;
+
+    }
+
+    return 0;
+}
