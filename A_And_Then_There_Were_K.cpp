f754f5e176dd7d7fb4cd91d5d77c095d82534576
muhammad-sayem <shahrulislamsayem@gmail.com>
2023-10-19 23:19:32 +0600

    Codeforces Round 721 (Div. 2) - 1527 A

---
+++ A_And_Then_There_Were_K.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+#define      endl            '\n'
+#define      yes             (cout << "YES\n")
+#define      no              (cout << "NO\n")
+#define      ll              long long
+#define      pb              push_back
+#define      pii             pair <int,int>
+#define      pll             pair <long long,long long>
+#define      rev(a)          reverse(a, a+n);
+#define      Srev(s)         reverse(s.begin(), s.end());
+#define      sz(s)           s.size()
+#define      gcd(a,b)        __gcd(a,b)
+#define      lcm(a,b)        (a*b)/gcd(a,b)
+#define      fastIO          ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+using namespace std;
+int main()
+{
+    fastIO;
+
+    int test;
+    cin >> test;
+
+    while(test--){
+        ll n, i, val, ans;
+        cin >> n;
+
+        for(i=1; i<=32; i++){
+            if(pow(2, i) > n){
+                val = i-1;
+                break;
+            }
+            else if(pow(2, i) == n){
+                val = i;
+                break;
+            }
+        }
+
+        ans = pow(2, val)-1;
+        
+        cout << ans << endl;
+    }
+
+    return 0;
+}
