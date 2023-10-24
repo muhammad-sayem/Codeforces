2ff2c71afaa42ea6106ff82b313f7e7651635f35
muhammad-sayem <shahrulislamsayem@gmail.com>
2023-10-24 16:22:27 +0600

    Codeforces Round 582 (Div. 3)

---
+++ A_Chips_Moving.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+#define      endl            '\n'
+#define      yes             (cout << "YES\n")
+#define      no              (cout << "NO\n")
+#define      ll              long long
+#define      pb              push_back
+#define      pii             pair <int,int>
+#define      pll             pair <long long,long long>
+#define      rev(a)          reverse(a, a+n);
+#define      Srev(s)         reverse(s.begin(), s.end());
+#define      sz(s)           s.size()
+#define      gcd(a,b)        __gcd(a,b)
+#define      lcm(a,b)        (a*b)/gcd(a,b)
+#define      fastIO          ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+using namespace std;
+int main()
+{
+    fastIO;
+
+    ll n, i, even = 0, odd = 0;
+    cin >> n;
+    ll a[n+5];
+
+    for(i=1; i<=n; i++){
+        cin >> a[i];
+        if(a[i]&1) odd++;
+        else even++;
+    }
+    cout << min(even, odd) << endl;
+
+    return 0;
+}
