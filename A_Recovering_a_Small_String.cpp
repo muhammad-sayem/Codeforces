cad319dd41fb3787e9d9e3f831263717925e427e
muhammad-sayem <shahrulislamsayem@gmail.com>
2024-02-15 21:31:14 +0600

    Codeforces Round 926 (Div. 2) - 1929A

---
+++ A_Recovering_a_Small_String.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+#define      nl              '\n'
+#define      yes             (cout << "YES\n")
+#define      no              (cout << "NO\n")
+#define      yy              (cout << "Yes\n")
+#define      nn              (cout << "No\n")
+#define      ll              long long
+#define      pb              push_back
+#define      pii             pair <int,int>
+#define      pll             pair <long long,long long>
+#define      rev(a)          reverse(a, a+n);
+#define      Srev(s)         reverse(s.begin(), s.end());
+#define      sz(s)           s.size()
+#define      gcd(a,b)        __gcd(a,b)
+#define      lcm(a,b)        (a*b)/gcd(a,b)
+#define      fastIO          ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+using namespace std;
+int main()
+{
+    fastIO;
+
+    int test;
+    cin >> test;
+
+    while(test--){
+        int n, i;
+        cin >> n;
+
+        int x = n/26;
+
+        if(x == 3){
+            cout << "zzz" << nl;
+        }
+        else if(x == 2){
+            int p = n-52;
+            char ch = p + 96;
+            cout << ch;
+            cout << "zz" << nl;
+        }
+        else if(x == 1){
+            int p = n-26-1;
+            cout << 'a';
+            char ch = p + 96;
+            cout << ch;
+            cout << 'z' << nl;
+        }
+        else{
+            int p = n-2;
+            char ch = p + 96;
+            cout << "aa";
+            cout << ch << nl;
+        }
+    }
+    
+    return 0;
+}
